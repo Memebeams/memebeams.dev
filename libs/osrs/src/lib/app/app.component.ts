a1a3f575dea2c7171e30108dc0b11f3d3cc3734a
Memebeams <5132037+Memebeams@users.noreply.github.com>
2024-03-18 18:06:15 -0400

    move tooltip to rs-app

--- libs/osrs/src/lib/app/app.component.ts
+++ libs/osrs/src/lib/app/app.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { OSRSButtonsComponent } from '../buttons/buttons.component';
+import { OSRSTooltipComponent } from '../tooltip/tooltip.component';
+import { OSRSTooltipService } from '../tooltip/tooltip.service';
 
 @Component({
   selector: 'rs-app',
   standalone: true,
-  imports: [CommonModule, OSRSButtonsComponent],
+  imports: [CommonModule, OSRSButtonsComponent, OSRSTooltipComponent],
+  providers: [OSRSTooltipService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
