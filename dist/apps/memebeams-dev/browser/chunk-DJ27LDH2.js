06529b2538bc3cabcf6c21cda95a631fb6c7054b
Gerhard <tetzner.gerhard@gmail.com>
2024-03-27 14:19:46 -0400

    Build for SOTW page

---
+++ dist/apps/memebeams-dev/browser/chunk-DJ27LDH2.js
@@ -0,0 +1,1 @@
+import{m as a,n as b,o as c,p as d,q as e}from"./chunk-PGWNBQIB.js";export{a as ClanBannerComponent,b as ClanBountyComponent,c as ClanDiscordComponent,d as ClanHomeComponent,e as ClanSotwComponent};
--- dist/apps/memebeams-dev/browser/chunk-H3ED5BQJ.js
+++
@@ -1,1 +0,0 @@
-import{m as a,n as b,o as c,p as d}from"./chunk-NHTSCWV7.js";export{a as ClanBannerComponent,b as ClanBountyComponent,c as ClanDiscordComponent,d as ClanHomeComponent};
