c630de975c963f771cb522ecd441fae1ce7025b5
Gerhard <tetzner.gerhard@gmail.com>
2024-03-27 14:18:15 -0400

    BOTW / SOTW

--- libs/clan/src/index.ts
+++ libs/clan/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/banner/banner.component';
 export * from './lib/bounty/bounty.component';
 export * from './lib/discord/discord.component';
 export * from './lib/home/home.component';
+export * from './lib/sotw/sotw.component';
